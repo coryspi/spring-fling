fc061634df1c6c8a31fe77d002cd2b84969c5bbb
Louis Lebow <lebowl@bxscience.edu>
2016-10-24 16:39:00 -0400

    added some comments to springsubsystem and made it so the talons reset to base position after a shot is taken

--- src/org/usfirst/frc/team1155/robot/commands/Fire.java
+++ src/org/usfirst/frc/team1155/robot/commands/Fire.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team1155.robot.commands;
 
+import org.usfirst.frc.team1155.robot.Robot;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -10,6 +12,7 @@ public class Fire extends Command {
     public Fire() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    	requires(Robot.springSubsystem);
     }
 
     // Called just before this Command runs the first time
@@ -27,6 +30,10 @@ public class Fire extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    for (int i =leftTalon.getDistance(); i > springSubsystem.encMin; i ){
+    	springSubsystem.leftTalon.set(-1);
+    	springSubsystem.rightTalon.set(1);
+    }
     }
 
     // Called when another command which requires one or more of the same
--- src/org/usfirst/frc/team1155/robot/subsystems/SpringSubsystem.java
+++ src/org/usfirst/frc/team1155/robot/subsystems/SpringSubsystem.java
@@ -8,7 +8,10 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class SpringSubsystem extends Subsystem {
     
 	public static CANTalon leftTalon, rightTalon;
+	// encMax is a meaningless value meant to represent the farthest distance reachable by the talons 
 	public static int encMax = 5;
+	// encMin is a meaningless value meant to represent the lowest distance reachable by the talons 
+	public static int encMin =0 ;
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
