9baefa7961fce6a98c85b7b519ed6bf4383cb2ad
Alexander Warren <arocks124@gmail.com>
2016-11-21 16:36:23 -0500

    Changed locks from talons to pistons

--- src/org/usfirst/frc/team1155/robot/subsystems/SpringSubsystem.java
+++ src/org/usfirst/frc/team1155/robot/subsystems/SpringSubsystem.java
@@ -7,7 +7,8 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class SpringSubsystem extends Subsystem {
     
-	public static CANTalon leftGearTalon, rightGearTalon, leftLockTalon, rightLockTalon;
+	public static CANTalon leftGearTalon, rightGearTalon;
+	public static Solenoid leftLockPiston, rightLockPiston;
 	// encMax is a meaningless value meant to represent the farthest distance reachable by the talons 
 	public static int encMax = 5;
 	// encMin is a meaningless value meant to represent the lowest distance reachable by the talons 
@@ -20,8 +21,12 @@ public class SpringSubsystem extends Subsystem {
 		
 		leftGearTalon = new CANTalon(0);
 		rightGearTalon = new CANTalon(1);
-		leftLockTalon = new CANTalon(3);
-		rightLockTalon = new CANTalon(4);
+		leftLockPiston = new Solenoid(3);
+		rightLockPiston = new Solenoid(4);
+		
+	}
+	
+	public void lock(){
 		
 	}
 	
